2125f1c0ec3ba1f40bc1a39902a583405d816571
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-22 04:53:57 -0700

    create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create file, with file permission, file owner, file group and file contents
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
