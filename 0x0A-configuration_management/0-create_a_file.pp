f73d6e6bd25fcc3a9d8edabe99467b25814ad8fe
Fikayomi31 <fikayoogundijo@gmail.com>
2023-09-23 08:57:20 +0000

    unstaged changes

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Create file, with file permission, file owner, file group and file contents
-
-file { '/tmp/school':
-  ensure  => 'file',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# Installing flask from pip3 and version must be 2.1.0
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => pip3,
-}
