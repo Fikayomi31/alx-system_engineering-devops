679c49f23ec63976da1178a81ad2914a88478d21
Fikayomi31 <fikayoogundijo@gmail.com>
2023-10-25 16:32:10 +0000

    gather data for API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Fetch and display information about an employee
-API https://jsonplaceholder.typicode.com/
+API: https://jsonplaceholder.typicode.com/
 """
 
 import requests
@@ -9,25 +9,23 @@ import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users"
+    url = api = "https://jsonplaceholder.typicode.com/users"
+
     response = requests.get("{}/{}".format(url, employee_id))
-
     employee_name = response.json().get('name')
-
-    """To get the todos """
+    
     todo_url = url + "/" + employee_id + "/todos"
     response = requests.get(todo_url)
     tasks = response.json()
     done = 0
-    done_task = []
+    done_tasks = []
 
     for task in tasks:
         if task.get('completed'):
-            done_task.append(task)
+            done_tasks.append(task)
             done += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_id, done, len(tasks)))
 
-    print("Employee {} is done with task({}/{}):"
-          .format(employee_name, done, len(tasks)))
-
-    for task in done_task:
+    for task in done_tasks:
         print("\t {}".format(task.get('title')))
