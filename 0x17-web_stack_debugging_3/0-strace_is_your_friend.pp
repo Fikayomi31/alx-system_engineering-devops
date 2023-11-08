dc062d345ed1aa4eab288a60114e2668c1df337d
Fikayomi31 <fikayoogundijo@gmail.com>
2023-11-08 16:50:09 +0000

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,6 @@
-# A puppet manuscript to replace a line in a file on a server
-
-$file_to_edit = '/var/www/html/wp-settings.php'
-
-#replace line containing "phpp" with "php"
+# Fix error 500 which is shelling error in a file
 
 exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
+  command  => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  provider => 'shell'
 }
