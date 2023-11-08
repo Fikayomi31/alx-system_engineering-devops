eadc1d4b44ed8fcaa8530b3179e0a3ad37feae7c
Fikayomi31 <fikayoogundijo@gmail.com>
2023-11-08 16:46:00 +0000

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,10 @@
-# Fix apache2 whicb is running error 50
+# A puppet manuscript to replace a line in a file on a server
 
-exec {'Fix error 500':
-  command  => 'sed i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  provider => 'shell'
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+#replace line containing "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
